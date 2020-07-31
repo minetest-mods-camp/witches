464d15c11d6fbf1a6b6d75f9ecba299093258dea
francisco athens <cyan255@gmail.com>
2020-07-31 15:35:34 -0700

    mutton texture fix

--- sheep.lua
+++ sheep.lua
@@ -235,7 +235,7 @@ mobs:spawn({
 -- raw mutton
 minetest.register_craftitem(":mobs:mutton_raw", {
 	description = S("Raw Mutton"),
-	inventory_image = "mobs_mutton_raw.png",
+	inventory_image = "mobs_meat_raw.png",
 	on_use = minetest.item_eat(2),
 	groups = {food_meat_raw = 1, food_mutton_raw = 1, flammable = 2},
 })
@@ -243,7 +243,7 @@ minetest.register_craftitem(":mobs:mutton_raw", {
 -- cooked mutton
 minetest.register_craftitem(":mobs:mutton_cooked", {
 	description = S("Cooked Mutton"),
-	inventory_image = "mobs_mutton_cooked.png",
+	inventory_image = "mobs_meat_cooked.png",
 	on_use = minetest.item_eat(6),
 	groups = {food_meat = 1, food_mutton = 1, flammable = 2},
 })
